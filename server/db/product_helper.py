afad67c2246cfb3e2d51c623e381fd22c00264ab
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-11-28 11:36:17 +0500

    ENH: get one prudct is refacotr

--- server/db/product_helper.py
+++ server/db/product_helper.py
@@ -1,6 +1,7 @@
 
 from fastapi import HTTPException
 from sqlalchemy import func, select, select
+from server.models.models import Product, ProductImage
 
 
 def helper_for_get_request(session, query, count_subquery, number):
@@ -34,3 +35,46 @@ def helper_for_get_request(session, query, count_subquery, number):
         products_with_images.append(product_with_images)
 
     return products_with_images
+
+def helper_for_get_one_product(session, id:int):
+    # Create a query to select the Product and ProductImage based on the provided ID
+    query = (
+        select(Product, ProductImage)
+        .join(ProductImage)
+        .filter(Product.id == id)
+        .order_by(Product.id)
+    )
+    # Execute the query and get the first result
+    result = session.execute(query).first()
+    # If no result is found, raise an HTTPException with a 404 status code
+    if result is None:
+        raise HTTPException(status_code=404, detail=f"Product with ID {id} not found")
+
+    # Extract the Product and ProductImage from the result
+    product, image = result
+
+    # Create a dictionary to store the product information
+    product_with_images = {
+        "id": product.id,
+        "product_name": product.product_name,
+        "description": product.description,
+        "price": product.price,
+        "stock_quantity": product.stock_quantity,
+        "product_size": product.product_size,
+        "SKU": product.SKU,
+        "target_audience": product.target_audience,
+        "product_color": product.product_color,
+        "created_at": product.created_at,
+        "category_id": product.category_id,
+        "images": [],
+    }
+
+    # If there is an image, add it to the product_with_images dictionary
+    if image is not None:
+        product_with_images["images"].append({
+            "image_path": image.image_path,
+        })
+    
+    # Return the product_with_images dictionary
+    return product_with_images
+    
--- server/routers/product_router.py
+++ server/routers/product_router.py
@@ -6,7 +6,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from server.schemas import product_schemas
 from sqlalchemy import select
 from sqlalchemy import func, select, outerjoin
-from server.db.product_helper import helper_for_get_request
+from server.db import product_helper
 router = APIRouter(prefix="/product", tags=["Product  CRUD"])
 
 @router.post("/create", status_code=status.HTTP_201_CREATED)
@@ -118,59 +118,6 @@ async def update_product(id: int, product_update: product_schemas.ProductUpadte 
     # Return the updated product
     return product_query.first()
 
-@router.get("/all")
-async def get_all_products():
-    """
-    Get all products with their images.
-
-    Parameters:
-    - db: SQLAlchemy Session dependency.
-
-    Returns:
-    - List of products with images.
-    """
-    # Use a join query to retrieve products with their images
-    query = (
-        select(Product, ProductImage)
-        .join(ProductImage)
-        .order_by(Product.id)
-    )
-    
-    # Execute the query and fetch all results
-    results = session.execute(query).fetchall()
-
-    # Create a dictionary to store products with their images
-    products_with_images = {}
-
-    # Iterate through the results and populate the dictionary
-    for product, image in results:
-        if product.id not in products_with_images:
-            # Create a new dictionary for the product if it doesn't exist
-            products_with_images[product.id] = {
-                "id": product.id,
-                "product_name": product.product_name,
-                "description": product.description,
-                "price": product.price,
-                "stock_quantity": product.stock_quantity,
-                "product_size": product.product_size,
-                "SKU": product.SKU,
-                "target_audience": product.target_audience,
-                "product_color": product.product_color,
-                "created_at": product.created_at,
-                "category_id": product.category_id,
-                "images": [],
-            }
-        
-        if image is not None:
-            # Append the image to the product's images list
-            products_with_images[product.id]["images"].append({
-                "image_path": image.image_path,
-            })
-
-    # Convert the dictionary values to a list for response
-    result_list = list(products_with_images.values())
-    
-    return result_list
 
 @router.get("/{id}")
 async def get_one_product(id: int):
@@ -183,46 +130,9 @@ async def get_one_product(id: int):
     Returns:
     - Product with images.
     """
-    # Create a query to select the Product and ProductImage based on the provided ID
-    query = (
-        select(Product, ProductImage)
-        .join(ProductImage)
-        .filter(Product.id == id)
-        .order_by(Product.id)
-    )
-    # Execute the query and get the first result
-    result = session.execute(query).first()
-    # If no result is found, raise an HTTPException with a 404 status code
-    if result is None:
-        raise HTTPException(status_code=404, detail=f"Product with ID {id} not found")
-
-    # Extract the Product and ProductImage from the result
-    product, image = result
-
-    # Create a dictionary to store the product information
-    product_with_images = {
-        "id": product.id,
-        "product_name": product.product_name,
-        "description": product.description,
-        "price": product.price,
-        "stock_quantity": product.stock_quantity,
-        "product_size": product.product_size,
-        "SKU": product.SKU,
-        "target_audience": product.target_audience,
-        "product_color": product.product_color,
-        "created_at": product.created_at,
-        "category_id": product.category_id,
-        "images": [],
-    }
-
-    # If there is an image, add it to the product_with_images dictionary
-    if image is not None:
-        product_with_images["images"].append({
-            "image_path": image.image_path,
-        })
-
-    # Return the product_with_images dictionary
-    return product_with_images
+    # Get the product with the given id but image it gets one image at a time and this issue needs to be fixed
+    data = product_helper.helper_for_get_one_product(session= session, id=id)
+    return data
     
 
 @router.get("getproducts/{number}")
@@ -252,7 +162,7 @@ async def get_product_up_to_given_number(number: int):
         .distinct(Product.id)
     )
 
-    data = helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
+    data = product_helper.helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
     return data
 
 @router.get("getbyname/{product_name}/{number}")
@@ -282,7 +192,7 @@ async def get_product_by_name(product_name: str, number: int):
         .order_by(Product.id)
         .distinct(Product.id)
     )
-    data = helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
+    data = product_helper.helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
     return data
 
 
@@ -313,7 +223,7 @@ async def get_product_by_name(category_id: int, number: int):
         .order_by(Product.id)
         .distinct(Product.id)
     )
-    data = helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
+    data = product_helper.helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
     return data
 
 
@@ -345,5 +255,5 @@ async def get_product_by_name(category_id: int, search_keyword: str, number: int
         .order_by(Product.id)
         .distinct(Product.id)
     )
-    data = helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
+    data = product_helper.helper_for_get_request(session=session, query=query, count_subquery=count_subquery, number=number)
     return data
