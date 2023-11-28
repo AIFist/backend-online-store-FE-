9925d9a85621d57ce5fe3484abb262f65bfbc027
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-11-28 10:34:41 +0500

    ENH: get products by category  keyword

--- server/routers/product_router.py
+++ server/routers/product_router.py
@@ -399,3 +399,62 @@ async def get_product_by_name(category_id: int, number: int):
         products_with_images.append(product_with_images)
 
     return products_with_images
+
+@router.get("getbycategory_keyword/{category_id}/{search_keyword}/{number}")
+async def get_product_by_name(category_id: int, search_keyword: str, number: int):
+    """
+    Get a multiple product with its images based on the provided product category.
+
+    Parameters:
+    - id: Product ID obtained from the path parameter.
+
+    Returns:
+    - Product with images.
+    """
+    # Create a subquery to count the rows
+    count_subquery = (
+        select([func.count()])
+        .select_from(outerjoin(Product, ProductImage))
+        .scalar_subquery()
+    )
+    # Create a query to select the Product and ProductImage based on the provided product name
+    # Create the main query
+    query = (
+        select(Product, ProductImage)
+        .outerjoin(ProductImage)
+        .filter(Product.category_id == category_id)
+        .filter(Product.product_name.contains(search_keyword))
+        .limit(number)
+        .order_by(Product.id)
+        .distinct(Product.id)
+    )
+    # Check if the number of rows in the table is less than the specified number
+    if session.execute(select([func.count()]).select_from(query.alias())).scalar() < number:
+        query = query.limit(count_subquery)  # Limit the main query by the count
+        
+    # Execute the query and get the results
+    result = session.execute(query).all()
+    # Execute the query and get the first result
+    # If no result is found, raise an HTTPException with a 404 status code
+    if result is None:
+        raise HTTPException(status_code=404, detail=f"Product with name {category_id} not found")
+    # Extract the Product and ProductImage from the result
+    products_with_images = []
+    for product, image in result:
+        product_with_images = {
+            "id": product.id,
+            "product_name": product.product_name,
+            "description": product.description,
+            "price": product.price,
+            "stock_quantity": product.stock_quantity,
+            "product_size": product.product_size,
+            "SKU": product.SKU,
+            "target_audience": product.target_audience,
+            "product_color": product.product_color,
+            "created_at": product.created_at,
+            "category_id": product.category_id,
+            "images": [{"id": image.id, "image_path": image.image_path} for image in product.images],
+        }
+        products_with_images.append(product_with_images)
+
+    return products_with_images
