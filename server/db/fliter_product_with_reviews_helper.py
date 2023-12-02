986fd564a6f33034fb83a9347610710543003616
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-02 16:33:47 +0500

    DOC: fliter_product_with_reviews_helper

--- server/db/fliter_product_with_reviews_helper.py
+++ server/db/fliter_product_with_reviews_helper.py
@@ -6,6 +6,15 @@ from sqlalchemy import func, select
 from sqlalchemy import or_
 
 def get_products_with_images_and_reviews(product_name, startindex, number):
+    """
+    Get products with images and reviews.
+    Args:
+        product_name (str): The name of the product to search for.
+        startindex (int): The starting index to retrieve the products.
+        number (int): The number of products to retrieve.
+    Returns:
+        Query: The SQLAlchemy query object.
+    """
     # Create the main query
     query = (
     select(
@@ -30,7 +39,6 @@ def get_products(number: int, startindex: int):
     Retrieves a list of products with their corresponding images.
 
     Args:
-        session: The database session to use.
         number: The maximum number of products to retrieve.
         startindex: The index of the first product to retrieve.
 
@@ -56,6 +64,18 @@ def get_products(number: int, startindex: int):
     return query
 
 def get_product_by_category(category_id: int,startindex: int, number: int):
+    """
+    Retrieve products by category with additional information:
+    - ProductImage
+    - Number of reviews
+    - Average rating
+    Args:
+    - category_id: int - the ID of the category
+    - startindex: int - the starting index for pagination
+    - number: int - the number of products to retrieve
+    Returns:
+    - query: SQLAlchemy query object
+    """
     query = (
     select(
         Product,
@@ -76,6 +96,16 @@ def get_product_by_category(category_id: int,startindex: int, number: int):
 
 
 def get_product_by_category_keyword(category_id: int, search_keyword: str, number: int, startindex: int):
+    """
+    Retrieve products based on category, search keyword, and pagination.
+    Args:
+        category_id (int): The ID of the category to filter the products by.
+        search_keyword (str): The keyword to search for in the product name and description.
+        number (int): The number of products to retrieve.
+        startindex (int): The starting index of the retrieved products.
+    Returns:
+        sqlalchemy.orm.query.Query: The query object to retrieve the products.
+    """
     query = (
     select(
         Product,
@@ -100,10 +130,10 @@ def search_product_by_productsize(product_size: str, number: int, startindex: in
     """
     Search products by product size and return a query object.
     Args:
-        session: The database session.
         product_size: The size of the product to search for.
         number: The number of products to retrieve.
         startindex: The starting index for retrieving products.
+    
     Returns:
         A query object that fetches products filtered by product size.
     """
@@ -131,7 +161,6 @@ def filter_product_by_price(min_price: float, max_price: float, number: int, pro
     """
     Filter products by price range and return a query object.
     Args:
-        session: The database session.
         min_price: The minimum price of the product to filter by.
         max_price: The maximum price of the product to filter by.
         number: The number of products to retrieve.
@@ -140,7 +169,7 @@ def filter_product_by_price(min_price: float, max_price: float, number: int, pro
     Returns:
         A query object that fetches products filtered by price range.
     """
-    # Call the helper function to execute the query and return the result
+    # Create the query object
     query = (
         select(
             Product,
