05eff67c32ff5c9a4347bb95d3139820372e3350
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-04 15:32:50 +0500

    ENH : top rateed some what working

--- server/db/landing_page_helper.py
+++ server/db/landing_page_helper.py
@@ -5,7 +5,7 @@ from sqlalchemy.orm import aliased
 from sqlalchemy import func, select
 
 
-def get_random(number_of_products: int):
+def get_random_products_helper(number_of_products: int):
     # Use aliased to create an alias for the Product table
     product_alias = aliased(Product)
 
@@ -32,6 +32,44 @@ def get_random(number_of_products: int):
         .group_by(product_alias, ProductImage, Sales.discount_percent)
         .order_by(product_alias.id)
         .distinct(product_alias.id)
+    )
+
+    return query
+
+
+
+def get_top_rated_products_helper(number_of_products: int = 5):
+    # Use aliased to create an alias for the Review table
+    review_alias = aliased(Review)
+
+    # Create a subquery to get the number of reviews and average rating for each product
+    review_info_subquery = (
+        select([
+            Product.id,
+            func.count(review_alias.id).label("num_reviews"),
+            func.avg(review_alias.rating).label("avg_rating")
+        ])
+        .outerjoin(review_alias)
+        .group_by(Product.id)
+    ).alias("review_info")
+
+    # Create the main query to retrieve product details with reviews and discounts
+    query = (
+        select([
+            Product,
+            ProductImage,
+            review_info_subquery.c.num_reviews,
+            review_info_subquery.c.avg_rating,
+            Sales.discount_percent.label("latest_discount_percent")
+        ])
+        .select_from(Product)  # Explicitly set the main table
+        .outerjoin(ProductImage)
+        .outerjoin(review_info_subquery, Product.id == review_info_subquery.c.id)
+        .outerjoin(Sales, Product.id == Sales.product_id)
+        .filter(review_info_subquery.c.num_reviews > 0)  # Only include products with reviews
+        .order_by(desc(review_info_subquery.c.avg_rating), desc(review_info_subquery.c.num_reviews))
+        .distinct(review_info_subquery.c.avg_rating, review_info_subquery.c.num_reviews, Product.id)  # Ensure distinct products in the result
+        .limit(number_of_products)
     )
 
     return query
--- server/routers/landing_page_router.py
+++ server/routers/landing_page_router.py
@@ -21,8 +21,7 @@ def get_random_products(number_of_products):
     Returns:
         List: A list of dictionaries containing product information.
     """
-    query = landing_page_helper.get_random(number_of_products=number_of_products)
-    
+    query = landing_page_helper.get_random_products_helper(number_of_products=number_of_products)
 
     data = helper_for_getting_data.helper_for_filters_with_review_and_discount(session=session, query=query)
     return data
@@ -41,65 +40,8 @@ def get_trending_products_with_reviews(number_of_products):
 
 @router.get("/gettopratedproducts/{number_of_products}")
 def get_top_rated_products(number_of_products: int):
-    try:
-        # Use aliased to create an alias for the Review table
-        review_alias = aliased(Review)
+    
+    query = landing_page_helper.get_top_rated_products_helper(number_of_products=number_of_products)
 
-        # Create a subquery to get the number of reviews and average rating for each product
-        review_info_subquery = (
-            select([
-                Product.id,
-                func.count(review_alias.id).label("num_reviews"),
-                func.avg(review_alias.rating).label("avg_rating")
-            ])
-            .outerjoin(review_alias)
-            .group_by(Product.id)
-        ).alias("review_info")
-
-        # Create the main query to retrieve product details with reviews and discounts
-        query = (
-            select([
-                Product,
-                ProductImage,
-                review_info_subquery.c.num_reviews,
-                review_info_subquery.c.avg_rating,
-                Sales.discount_percent.label("latest_discount_percent")
-            ])
-            .select_from(Product)  # Explicitly set the main table
-            .outerjoin(ProductImage)
-            .outerjoin(review_info_subquery, Product.id == review_info_subquery.c.id)
-            .outerjoin(Sales, Product.id == Sales.product_id)
-            .order_by(desc(review_info_subquery.c.avg_rating))
-            .limit(number_of_products)
-        )
-
-        # Execute the query and fetch the result
-        result = session.execute(query).all()
-    except SQLAlchemyError as e:
-        print(f"An error occurred: {e}")
-        # Handle the error or log it as needed
-        return {"error": "Internal Server Error"}
-
-    # Extract the product details from the result
-    top_rated_products = [
-        {
-            "id": product.id,
-            "product_name": product.product_name,
-            "description": product.description,
-            "price": product.price,
-            "stock_quantity": product.stock_quantity,
-            "product_size": product.product_size,
-            "SKU": product.SKU,
-            "target_audience": product.target_audience,
-            "product_color": product.product_color,
-            "created_at": product.created_at,
-            "category_id": product.category_id,
-            "images": [{"id": image.id, "image_path": image.image_path} for image in product.images],
-            "num_reviews": num_reviews,
-            "avg_rating": avg_rating,
-            "latest_discount_percent": latest_discount_percent,
-        }
-        for product, image, num_reviews, avg_rating, latest_discount_percent in result
-    ]
-
-    return top_rated_products
+    data = helper_for_getting_data.helper_for_filters_with_review_and_discount(session=session, query=query)
+    return data
