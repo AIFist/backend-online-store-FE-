a3baefd8ab184ab221fca7fed55df46adf403e44
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-11 13:51:43 +0500

    ENH: delete_product_image is added

--- server/db/product_image_helper.py
+++ server/db/product_image_helper.py
@@ -1,6 +1,8 @@
 from server.models.models import ProductImage
 from sqlalchemy.exc import SQLAlchemyError
 from server.schemas import product_image_schemas
+from fastapi import status,HTTPException, Response
+
 def helper_create_product_image(session, product_image:product_image_schemas.ProductImageCreate ):
     try:
         # Create a new ProductCategory instance using the data from the product_category model
@@ -28,3 +30,34 @@ def helper_create_product_image(session, product_image:product_image_schemas.Pro
     
     # Return the newly created ProductCategory
     return new_product_image
+
+
+def helper_delete_product_image(session, id: int):
+    """
+    Deletes a product category by ID.
+
+    Args:
+        session: The database session.
+        id: The ID of the product category to delete.
+
+    Raises:
+        HTTPException: If the product category does not exist.
+
+    Returns:
+        Response: A response with no content.
+    """
+    # Query the product category by ID
+    product_cat_query = session.query(ProductImage).filter(ProductImage.id == id)
+    product_cat = product_cat_query.first()
+
+    # Check if the product category exists
+    if product_cat is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"Product Category with id {id} does not exist")
+
+    # Delete the product category
+    product_cat_query.delete(synchronize_session=False)
+    session.commit()
+
+    # Return a response with no content
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
--- server/routers/product_image_router.py
+++ server/routers/product_image_router.py
@@ -24,3 +24,17 @@ async def create_product_image(product_image: product_image_schemas.ProductImage
     """
     data = product_image_helper.helper_create_product_image(session=session, product_image=product_image)
     return data
+
+@router.delete("/{id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_product_image(id: int):
+    """
+    Delete a product image by its ID.
+
+    Args:
+        id (int): The ID of the product image to be deleted.
+
+    Raises:
+        HTTPException: If the product image with the given ID does not exist.
+    """
+    return product_image_helper.helper_delete_product_image(session=session, id=id)
+    
