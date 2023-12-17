202081fd4e1a6f0844b676437314ced713a879e3
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-17 13:12:24 +0500

    docs: docs are added in login endpoint

--- server/routers/auth_router.py
+++ server/routers/auth_router.py
@@ -10,17 +10,19 @@ router = APIRouter(tags=['Authentication'])
 
 @router.post('/login', response_model=token_schemas.Token)
 def login(user_credentials: OAuth2PasswordRequestForm = Depends()):
-    user = db.query(User).filter(
-        User.email == user_credentials.username).first()
+    # Query the database to find the user with the given email
+    user = db.query(User).filter(User.email == user_credentials.username).first()
 
+    # If user is not found, raise an HTTPException with a 403 status code and an error message
     if not user:
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Invalid credentials")
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+
+    # If the password is incorrect, raise an HTTPException with a 403 status code and an error message
     if not hash_helper.verify(user_credentials.password, user.password):
-        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN,
-                            detail="Invalid credentials")
-    
-    # Generate JWT token but Ishaque have to add user role in jwt token
+        raise HTTPException(status_code=status.HTTP_403_FORBIDDEN, detail="Invalid credentials")
+
+    # Generate an access token using the oauth2 library, passing the user_id as data
     access_token = oauth2.create_access_token(data={"user_id": user.id})
 
+    # Return the access token and the token type as a dictionary
     return {"access_token": access_token, "token_type": "bearer"}
