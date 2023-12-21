10534ec1bafe65a86156ef1b71885089df4f3b41
MuhammadNizamani <ishaque.niz786@gmail.com>
2023-12-21 16:06:21 +0500

    ENH: added sfeatured

--- server/routers/featured_product_router.py
+++ server/routers/featured_product_router.py
@@ -1,9 +1,9 @@
-from fastapi import Body, status
+from fastapi import Body, status, Depends, HTTPException
 from fastapi.routing import APIRouter
 from server.models.models1 import session
 from server.schemas import featured_products_schemas
 from server.db import featured_helper
-
+from server.utils import oauth2
 router = APIRouter(prefix="/featured", tags=["featured Product CRUD"])
 
 @router.post("/create", 
@@ -11,7 +11,8 @@ router = APIRouter(prefix="/featured", tags=["featured Product CRUD"])
              response_model=featured_products_schemas.FeaturedProductCreateResponse
              )
 async def create_featured_product(
-    featured_product: featured_products_schemas.FeaturedProductCreate = Body(...)
+    featured_product: featured_products_schemas.FeaturedProductCreate = Body(...),
+    current_user: int = Depends(oauth2.get_current_user),
 ):
     """
     Creates a new featured product.
@@ -23,6 +24,12 @@ async def create_featured_product(
         The created featured product data.
     """
 
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to perform this action."
+        )
+    
     # Create the featured product in the database
     data = featured_helper.helper_create_featured_product(
         session=session, featured_product=featured_product
@@ -30,7 +37,10 @@ async def create_featured_product(
     return data
 
 @router.delete("/delete/{featured_product_id}")
-async def delete_featured_product(featured_product_id: int):
+async def delete_featured_product(
+    featured_product_id: int,
+    current_user: int = Depends(oauth2.get_current_user),
+    ):
     """
     Deletes a featured product.
 
@@ -41,6 +51,11 @@ async def delete_featured_product(featured_product_id: int):
         None
     """
 
+    if current_user.role != "admin":
+        raise HTTPException(
+            status_code=status.HTTP_403_FORBIDDEN,
+            detail="You do not have permission to perform this action."
+        )
     # Delete the featured product from the database
     res = featured_helper.helper_delete_featured_product(
         session=session, featured_product_id=featured_product_id
